9664cade57fb0553ae019093cadf7ab8264e8967
Doug Bryant <doug@netinlet.com>
2014-05-05 15:34:31 -0400

    bug fixes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,34 +2,40 @@
 
 
 angular.module('prorataApp')
-  .controller('MainCtrl', function ($scope, Calculator) {
+.controller('MainCtrl', function ($scope, Calculator) {
 
-    function calculateExpirationDate(newVal, oldVal, scope) {
-      if (scope.prorataCalculation.effectiveDate && scope.prorataCalculation.termInMonths) {
-        var effDate = Date.create(scope.prorataCalculation.effectiveDate);
-        scope.prorataCalculation.expirationDate = effDate.advance({months: scope.prorataCalculation.termInMonths}).format('{yyyy}-{MM}-{dd}');
-      }
+  $scope.calculateExpirationDate = function() {
+    if ($scope.prorataCalculation.effectiveDate && $scope.prorataCalculation.termInMonths) {
+      var effDate = Date.create($scope.prorataCalculation.effectiveDate);
+      $scope.prorataCalculation.expirationDate = effDate.advance({months: $scope.prorataCalculation.termInMonths}).format('{yyyy}-{MM}-{dd}');
+    }
+  }
+
+  $scope.prorataCalculation = {
+    termInMonths: 12,
+    factorMethod: 'standard'
+  };
+
+  $scope.calculateTermInMonths = function() {
+    if ($scope.prorataCalculation.effectiveDate && $scope.prorataCalculation.expirationDate) {
+      var effDate = Date.create($scope.prorataCalculation.effectiveDate);
+      var expDate = Date.create($scope.prorataCalculation.expirationDate);
+      $scope.prorataCalculation.termInMonths = expDate.monthsSince(effDate);
     }
 
-    $scope.prorataCalculation = {
-      termInMonths: 12,
-      factorMethod: 'standard'
-    };
+  };
 
-    $scope.prorataResult = { };
+  $scope.prorataResult = { };
 
-    $scope.recalculate = function() {
-      console.log('Recalculate Called...');
+  $scope.recalculate = function() {
+    console.log('Recalculate Called...');
 
-      var effDate    = Date.create(this.prorataCalculation.effectiveDate);
-      var expDate    = Date.create(this.prorataCalculation.expirationDate);
-      var cancelDate = Date.create(this.prorataCalculation.cancellationDate);
+    var effDate    = Date.create($scope.prorataCalculation.effectiveDate);
+    var expDate    = Date.create($scope.prorataCalculation.expirationDate);
+    if ($scope.prorataCalculation.cancellationDate) {
+      var cancelDate = Date.create($scope.prorataCalculation.cancellationDate);
+    }
+    $scope.prorataResult = Calculator.calculateProRata(effDate, expDate, cancelDate, $scope.prorataCalculation.premiumAmount, $scope.prorataCalculation.factorMethod);
+  };
 
-      $scope.prorataResult = Calculator.calculateProRata(effDate, expDate, cancelDate, this.prorataCalculation.premiumAmount, this.prorataCalculation.factorMethod);
-    };
-
-    $scope.$watch('prorataCalculation.effectiveDate', calculateExpirationDate );
-    $scope.$watch('prorataCalculation.termInMonths', calculateExpirationDate );
-
-
-  });
+});
